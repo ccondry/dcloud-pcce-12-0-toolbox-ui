951b32489bd154f076223361ff3a2d484b25119d
Coty Condry <ccondry@cisco.com>
2020-05-21 05:24:06 -0500

    remove unused admin actions. these features were moved to toolbox-management-ui

--- client/store/modules/admin.js
+++ client/store/modules/admin.js
@@ -21,59 +21,6 @@ const mutations = {
 }
 
 const actions = {
-  async loadUsers ({getters, commit, dispatch}, showNotification = true) {
-    dispatch('setLoading', {group: 'admin', type: 'users', value: true})
-    try {
-      await dispatch('loadToState', {
-        name: 'users',
-        endpoint: getters.endpoints.admin.users,
-        mutation: types.SET_USERS,
-        showNotification
-      })
-    } catch (e) {
-      console.log('error loading users', e)
-      dispatch('errorNotification', {title: 'Failed to load users', error: e})
-    } finally {
-      dispatch('setLoading', {group: 'admin', type: 'users', value: false})
-    }
-  },
-  async loadUserProvisionMap ({getters, commit, dispatch}, showNotification = true) {
-    dispatch('setLoading', {group: 'admin', type: 'userProvisionMap', value: true})
-    try {
-      await dispatch('loadToState', {
-        name: 'user provision map',
-        endpoint: getters.endpoints.admin.userProvisionMap,
-        mutation: types.SET_USER_PROVISION_MAP,
-        showNotification
-      })
-    } catch (e) {
-      console.log('error loading user provision map', e)
-      dispatch('errorNotification', {title: 'Failed to load user provision map', error: e})
-    } finally {
-      dispatch('setLoading', {group: 'admin', type: 'userProvisionMap', value: false})
-    }
-  },
-  async su ({getters, commit, dispatch}, {showNotification = true, user}) {
-    const type = 'su'
-    try {
-      dispatch('setWorking', {group: 'admin', type: 'su', value: true})
-      const endpoint = getters.endpoints.admin.su
-      const response = await post(getters.instanceName, getters.jwt, endpoint, null, {username: user.username})
-      // store auth token in localStorage
-      dispatch('setJwt', response.data.jwt)
-
-      // reload new user data using JWT
-      dispatch('loadUser')
-      if (showNotification) {
-        dispatch('successNotification', `Successfully logged in as ${user.username}`)
-      }
-    } catch (e) {
-      console.log(`error posting ${type}`, e)
-      dispatch('errorNotification', {title: `Failed to log in as ${user.username}`, error: e})
-    } finally {
-      dispatch('setWorking', {group: 'admin', type: 'su', value: false})
-    }
-  },
   async updateUser ({getters, commit, dispatch}, {showNotification = true, user, body}) {
     const type = 'updateUser'
     try {
